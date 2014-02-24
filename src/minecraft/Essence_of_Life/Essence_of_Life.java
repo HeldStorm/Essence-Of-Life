b896dfa2eaa27a6067e0558cb8aed7923dd44a99
HeldStorm <lrand47@gmail.com>
2014-02-24 17:52:30 +1000

    tom help me
    my game keeps crashing

--- src/minecraft/Essence_of_Life/Essence_of_Life.java
+++ src/minecraft/Essence_of_Life/Essence_of_Life.java
@@ -3,8 +3,10 @@ package Essence_of_Life;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import Essence_of_Life.CommonProxy;
+import Essence_of_Life_CreativeTab.tabEOL;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -15,7 +17,6 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-import Essence_of_Life_CreativeTab.*;
  
 @Mod(modid="Essence of Life", name="Essence of Life", version="0.0.1")
 @NetworkMod(clientSideRequired=true, serverSideRequired=true)
@@ -32,12 +33,14 @@ public class Essence_of_Life  {
     public static CommonProxy proxy;
     public CreativeTabs tabEOL = new tabEOL("Essence of Life");
     
-    public Block TitaniumOre;
+    public static Block TitaniumOre;
+    public static Item Titanium_Ingot;
     
    
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-    	TitaniumOre = new TitaniumOre(502, Material.rock).setHardness(3.0f).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("TitaniumOre").setCreativeTab(this.tabEOL).setTextureName("EOL:TitaniumOre");
+    	TitaniumOre = new TitaniumOre(502, Material.rock).setHardness(3.0f).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("TitaniumOre").setCreativeTab(this.tabEOL).setTextureName("eol:titaniumore");
+    	Titanium_Ingot = new Titanium_Ingot(501).setMaxStackSize(64).setCreativeTab(this.tabEOL).setUnlocalizedName("TitaniumIngott").setTextureName("eol:titanium_ingot");
     }
    
     @EventHandler
@@ -48,9 +51,13 @@ public class Essence_of_Life  {
             GameRegistry.registerBlock(TitaniumOre, "TitaniumOre");
             LanguageRegistry.addName(TitaniumOre, "Titanium Ore");
             MinecraftForge.setBlockHarvestLevel(TitaniumOre, "pickaxe", 2);
+            
+            GameRegistry.registerItem(Titanium_Ingot,"Titanium_Ingot");
+            LanguageRegistry.addName(Titanium_Ingot, "Titanium Ingot");
+            GameRegistry.addSmelting(TitaniumOre, 1, Titanium_Ingot, 0.7f);
     }
-   
-    @EventHandler
+
+	@EventHandler
     public void postInit(FMLPostInitializationEvent event) {
             // Stub Method
     }
---
+++ src/minecraft/Essence_of_Life/Titanium_Ingot.java
@@ -0,0 +1,15 @@
+package Essence_of_Life;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class Titanium_Ingot extends Item {
+
+	private CreativeTabs tabEOL;
+
+	public Titanium_Ingot(int par1) {
+		super(par1);
+		
+	}
+
+}
