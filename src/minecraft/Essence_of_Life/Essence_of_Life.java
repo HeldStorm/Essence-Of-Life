f4c81420d0a1fca9d857b723652938dda1dc0109
HeldStorm <lrand47@gmail.com>
2014-02-26 18:18:50 +1000

    life core
    i have added in the base life core that can be made with a block of
    diamond and 8 blocks of redstone my client crashes with no errors but
    it should work for others (tom can you check this out for me please?)

--- src/minecraft/Essence_of_Life/Essence_of_Life.java
+++ src/minecraft/Essence_of_Life/Essence_of_Life.java
@@ -39,6 +39,7 @@ public class Essence_of_Life  {
     public static Item Obsidian_Ingot;
     public static Item Steel_Ingot;
     public static Item Black_Hardened_Steel;
+    public static Item Life_Core;
     
     EventManager oreManager = new EventManager();
    
@@ -50,6 +51,7 @@ public class Essence_of_Life  {
     	Obsidian_Ingot = new Obsidian_Ingot(503).setMaxStackSize(64).setCreativeTab(this.tabEOL).setUnlocalizedName("Obsidian_Ingot").setTextureName("eol:obsidian_ingot");
     	Steel_Ingot = new Steel_Ingot(504).setMaxStackSize(64).setCreativeTab(this.tabEOL).setUnlocalizedName("steel_Ingot").setTextureName("eol:steel_ingot");
     	Black_Hardened_Steel = new Black_Hardened_Steel(506).setMaxStackSize(64).setCreativeTab(this.tabEOL).setUnlocalizedName("Black_Hardened_Steel").setTextureName("eol:bhs");
+    	Life_Core = new Life_Core(507).setMaxStackSize(1).setCreativeTab(this.tabEOL).setUnlocalizedName("LifeCore").setTextureName("eol:life_core");
     }
    
     private Object setMaxStackSize(int i) {
@@ -84,17 +86,22 @@ public class Essence_of_Life  {
             GameRegistry.registerItem(Black_Hardened_Steel, "Black_Hardened_Steel");
             LanguageRegistry.addName(Black_Hardened_Steel, "Black Hardened Steel");
             
+            GameRegistry.registerItem(Life_Core, "LifeCore");
+            LanguageRegistry.addName(Life_Core, "Life Core");
+            
             //this is an itemstack!
             ItemStack Ti_Ingot = new ItemStack(Titanium_Ingot);
             ItemStack Ob_Ingot = new ItemStack(Obsidian_Ingot);
             ItemStack St_Ingot = new ItemStack(Steel_Ingot);
             ItemStack BHS = new ItemStack(Black_Hardened_Steel);
+            ItemStack LC = new ItemStack(Life_Core);
             
             //you need to give it the block id and then the itemstack!!
             GameRegistry.addSmelting(TitaniumOre.blockID, Ti_Ingot, 0.7f);
             GameRegistry.addSmelting(Block.obsidian.blockID, Ob_Ingot, 0.7f);
             GameRegistry.addSmelting(SteelOre.blockID, St_Ingot, 0.7f);
             GameRegistry.addRecipe(BHS, "xxx", "yyy", "zzz", 'x', Ti_Ingot, 'y', Ob_Ingot, 'z', St_Ingot);
+            GameRegistry.addRecipe(LC, "yyy", "yxy", "yyy", 'x', Block.blockDiamond.blockID, 'y', Block.blockRedstone.blockID);
             
             GameRegistry.registerWorldGenerator(oreManager);
     }
---
+++ src/minecraft/Essence_of_Life/Life_Core.java
@@ -0,0 +1,11 @@
+package Essence_of_Life;
+
+import net.minecraft.item.Item;
+
+public class Life_Core extends Item {
+
+	public Life_Core(int par1) {
+		super(par1);
+	}
+
+}
